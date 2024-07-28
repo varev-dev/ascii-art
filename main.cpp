8cfaaf55d841287919294355657e5681355362e7
varev <varev@pop-os.lan>
2024-07-28 22:59:05 +0200

    create project, add OpenCV, test example code

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <opencv4/opencv2/opencv.hpp>
+#include <iostream>
+
+int main() {
+    cv::Mat image = cv::imread("input.jpg");
+
+    if (image.empty()) {
+        std::cout << "Could not open or find the image" << std::endl;
+        return -1;
+    }
+
+    // Accessing and modifying pixel values
+    for (int y = 0; y < image.rows; y++) {
+        for (int x = 0; x < image.cols; x++) {
+            // Get the pixel
+            auto &pixel = image.at<cv::Vec3b>(y, x);
+
+            // Modify the pixel (e.g., invert the color)
+            pixel[0] = 255 - pixel[0]; // Blue
+            pixel[1] = 255 - pixel[1]; // Green
+            pixel[2] = 255 - pixel[2]; // Red
+        }
+    }
+
+    // Save the modified image
+    cv::imwrite("output.jpg", image);
+
+    return 0;
+}
